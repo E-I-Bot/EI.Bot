944c81a1b4f7cbde78fb6a7934c0a85d36a58252
Jason <uncle.jason@qq.com>
2024-09-06 18:28:01 +0800

    Update Footer.js

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -22,6 +22,10 @@ export const Footer = (props) => {
          <p>
            <i class="fa-solid fa-face-grin-hearts"></i> 
            <a href='mailto:uncle.jason@qq.com' className='underline text-gray-500 dark:text-gray-300'> Interested in this Domain? </a>
+           <i class="fa-solid fa-robot"></i>
+           <i class="fa-solid fa-rocket"></i>
+           <i class="fa-solid fa-brain"></i>
+           If you are Elon Musk, get it for free！
          </p>
        </div>
      </div>
